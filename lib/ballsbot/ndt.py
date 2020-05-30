d202c5b26063ec3188883d886b4dabde74c785eb
jumpercc <jumper@jumper.cc>
2020-05-31 01:25:18 +0300

    update

--- lib/ballsbot/ndt.py
+++ lib/ballsbot/ndt.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import floor
 from math import cos, sin, pi, exp
 from scipy.optimize import minimize
-from scipy.linalg import eigvals
+from scipy.linalg import eigvals, eig
 
 
 class NDT:
@@ -22,38 +22,73 @@ class NDT:
         self.grad2_cache = {}
         self.steps = []
 
-    def get_cell_indexes(self, x, y):
-        if x < -self.grid_size:
-            x = self.shift_fix - self.grid_size
-        elif x > self.grid_size:
-            x = self.grid_size - self.shift_fix
-        if y < -self.grid_size:
-            y = self.shift_fix - self.grid_size
-        elif y > self.grid_size:
-            y = self.grid_size - self.shift_fix
-        return self.cells_count + floor(x / self.box_size), self.cells_count + floor(y / self.box_size)
+    def get_cell_indexes(self, x, y, shift_x=0., shift_y=0.):
+        if x < -self.grid_size + shift_x:
+            x_index = 0
+        elif x > self.grid_size + shift_x:
+            x_index = 2 * self.cells_count - 1
+        else:
+            x_index = self.cells_count + floor((x - shift_x) / self.box_size)
+
+        if y < -self.grid_size + shift_y:
+            y_index = 0
+        elif y > self.grid_size + shift_y:
+            y_index = 2 * self.cells_count - 1
+        else:
+            y_index = self.cells_count + floor((y - shift_y) / self.box_size)
+
+        return x_index, y_index
+
+    def get_all_cell_indexes(self, x, y):
+        return [
+            self.get_cell_indexes(x, y, shift_x=0., shift_y=0.),
+            self.get_cell_indexes(x, y, shift_x=self.box_size, shift_y=0.),
+            self.get_cell_indexes(x, y, shift_x=0., shift_y=self.box_size),
+            self.get_cell_indexes(x, y, shift_x=self.box_size, shift_y=self.box_size),
+        ]
+
+    def _fix_covariation(self, covar):
+        # To prevent this effect, we check, whether
+        # the smaller eigenvalue of Σ is at least 0.001 times the
+        # larger eigenvalue. If not, it is set to this value.
+        min_covar_eigvalue_mult = 0.001
+        eigenvalues, eigtnvectors = eig(covar)
+        if eigenvalues[0] < min_covar_eigvalue_mult * eigenvalues[1]:
+            diag_eig_values = np.array([
+                [eigenvalues[0].real, 0],
+                [0, eigenvalues[1].real]
+            ])
+            eig_bectors_m = np.array(eigtnvectors)
+            # set minimum smallest eigenvalue:
+            diag_eig_values[0, 0] = diag_eig_values[1, 1] * min_covar_eigvalue_mult
+            covar = eig_bectors_m @ diag_eig_values @ eig_bectors_m.T
+        return covar
 
     def cell_cloud(self, a_cloud):
-        points = {}
+        points = [{}, {}, {}, {}]
         x_points, y_points = a_cloud
         for i in range(len(x_points)):
             x = x_points[i]
             y = y_points[i]
-            key = self.get_cell_indexes(x, y)
-            if key not in points:
-                points[key] = [[], []]
-            points[key][0].append(x)
-            points[key][1].append(y)
+            for j, key in enumerate(self.get_all_cell_indexes(x, y)):
+                if key not in points[j]:
+                    points[j][key] = [[], []]
+                points[j][key][0].append(x)
+                points[j][key][1].append(y)
 
-        mean_m = np.zeros((2 * self.cells_count, 2 * self.cells_count, 2))
-        cov_m = np.zeros((2 * self.cells_count, 2 * self.cells_count, 2, 2))
-        for key, cell_points in points.items():
-            if len(cell_points[0]) < 3:
-                continue
-            i, j = key
-            mean_m[i, j] = np.mean(cell_points, axis=1)
-            cov_m[i, j] = np.cov(cell_points)
-        return mean_m, cov_m
+        results = []
+        for plist in points:
+            mean_m = np.zeros((2 * self.cells_count, 2 * self.cells_count, 2))
+            cov_m = np.zeros((2 * self.cells_count, 2 * self.cells_count, 2, 2))
+            for key, cell_points in plist.items():
+                if len(cell_points[0]) < 3:
+                    continue
+                i, j = key
+                mean_m[i, j] = np.mean(cell_points, axis=1)
+                cov_m[i, j] = self._fix_covariation(np.cov(cell_points))
+
+            results.append({'mean': mean_m, 'cov': cov_m})
+        return results
 
     @staticmethod
     def apply_transformation(x, y, tr, cos_fi, sin_fi):
@@ -111,7 +146,7 @@ class NDT:
             -tx * sin(fi) - ty * cos(fi)
         ])
 
-    def _get_score_and_gradients(self, tr, a_cloud, mean_one, inv_cov_one):
+    def _get_score_and_gradients(self, tr, a_cloud, grids):
         tr_key = tuple(tr)
         if tr_key not in self.score_cache:
             rotated_cloud_two = self.apply_transformation_to_cloud(a_cloud, tr)
@@ -127,29 +162,30 @@ class NDT:
 
             for point_index in range(len(x_points)):
                 a_point = np.array([x_points[point_index], y_points[point_index]])
-                x_index, y_index = self.get_cell_indexes(*a_point)
-                mean_v = mean_one[x_index, y_index]
-                inv_cov_v = inv_cov_one[x_index, y_index]
-                if np.count_nonzero(mean_v) == 0:
-                    continue
+                for i, loc in enumerate(self.get_all_cell_indexes(*a_point)):
+                    x_index, y_index = loc
+                    mean_v = grids[i]['mean'][x_index, y_index]
+                    inv_cov_v = grids[i]['cov'][x_index, y_index]
+                    if np.count_nonzero(mean_v) == 0:
+                        continue
 
-                q = a_point - mean_v
-                qt_cvi = q.T @ inv_cov_v
-                exp_qt_cvi_q = exp(-0.5 * qt_cvi @ q)
-                score -= exp_qt_cvi_q
+                    q = a_point - mean_v
+                    qt_cvi = q.T @ inv_cov_v
+                    exp_qt_cvi_q = exp(-0.5 * qt_cvi @ q)
+                    score -= exp_qt_cvi_q
 
-                for i in range(3):
-                    grad[i] += qt_cvi @ mean_jacobi_m[i] * exp_qt_cvi_q
+                    for i in range(3):
+                        grad[i] += qt_cvi @ mean_jacobi_m[i] * exp_qt_cvi_q
 
-                for i in range(3):
-                    for j in range(3):
-                        # second derivative only for i == j == 2:
-                        hess_item = mean_hess_m_item if (i == j == 2) else empy_hess_item
-                        grad2[i, j] += -exp_qt_cvi_q * (
-                                (-qt_cvi @ mean_jacobi_m[i]) * (-qt_cvi @ mean_jacobi_m[j])
-                                + (-qt_cvi @ hess_item)
-                                + (mean_jacobi_m[j].T @ inv_cov_v @ mean_jacobi_m[i])
-                        )
+                    for i in range(3):
+                        for j in range(3):
+                            # second derivative only for i == j == 2:
+                            hess_item = mean_hess_m_item if (i == j == 2) else empy_hess_item
+                            grad2[i, j] += -exp_qt_cvi_q * (
+                                    (-qt_cvi @ mean_jacobi_m[i]) * (-qt_cvi @ mean_jacobi_m[j])
+                                    + (-qt_cvi @ hess_item)
+                                    + (-mean_jacobi_m[j].T @ inv_cov_v @ mean_jacobi_m[i])
+                            )
 
             if score != 0.:
                 min_eigenvalue = 0.
@@ -170,18 +206,18 @@ class NDT:
             self.score_cache[tr_key] = score
             self.grad_cache[tr_key] = grad
             self.grad2_cache[tr_key] = grad2
-            self.steps.append([tr[0], tr[1], score, grad])
+            self.steps.append([tr[0], tr[1], tr[2], score, grad])
 
         return self.score_cache[tr_key], self.grad_cache[tr_key], self.grad2_cache[tr_key]
 
-    def score_transformation(self, tr, a_cloud, mean_one, inv_cov_one):
-        return self._get_score_and_gradients(tr, a_cloud, mean_one, inv_cov_one)[0]
+    def score_transformation(self, *args):
+        return self._get_score_and_gradients(*args)[0]
 
-    def get_gradient_vector(self, tr, a_cloud, mean_one, inv_cov_one):
-        return self._get_score_and_gradients(tr, a_cloud, mean_one, inv_cov_one)[1]
+    def get_gradient_vector(self, *args):
+        return self._get_score_and_gradients(*args)[1]
 
-    def get_gradient2_matrix(self, tr, a_cloud, mean_one, inv_cov_one):
-        return self._get_score_and_gradients(tr, a_cloud, mean_one, inv_cov_one)[2]
+    def get_gradient2_matrix(self, *args):
+        return self._get_score_and_gradients(*args)[2]
 
     def get_optimal_transformation(self, cloud_one, cloud_two, start_point=(0., 0., 0.)):
         self.transformed_cache = {}
@@ -189,21 +225,20 @@ class NDT:
         self.grad_cache = {}
         self.grad2_cache = {}
         self.steps = []
-        mean_one, cov_one = self.cell_cloud(cloud_one)
-        inv_cov_one = self.inv_cov(cov_one)
+        grids = self.cell_cloud(cloud_one)
+        for it in grids:
+            it['cov'] = self.inv_cov(it['cov'])
         return minimize(
             self.score_transformation,
             np.array(start_point),  # TODO better start point from EKF
-            args=(cloud_two, mean_one, inv_cov_one),
-            #bounds=((-1., 1.), (-1., 1.), (-1., 1.)),
-            #method='Newton-CG',
+            args=(cloud_two, grids),
+            # method='Newton-CG',
             method='CG',
             jac=self.get_gradient_vector,
-            #hess=self.get_gradient2_matrix,
+            # hess=self.get_gradient2_matrix,
             options={
-                # 'xtol': 0.05,
-                # 'gtol': 0.05,
+                'xtol': 0.1,  # FIXME
                 'disp': True,
-                #'maxiter': 3,
+                # 'maxiter': 1,
             }
         ).x
