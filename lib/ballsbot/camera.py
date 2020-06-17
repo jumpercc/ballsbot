2bc9cf9a129cd5a2b318e9339f34f28cff828607
jumpercc <jumper@jumper.cc>
2020-06-17 17:13:20 +0300

    csi camera

--- lib/ballsbot/camera.py
+++ lib/ballsbot/camera.py
@@ -9,39 +9,40 @@ import numpy as np
 from ballsbot.utils import keep_rps, bgr8_to_jpeg
 
 
-def get_captures(cameras_count):
-    if cameras_count <= 0:
-        raise ValueError('no cameras specified')
-    caps = []
-    for cam_id in range(0, 3):
-        cap = cv2.VideoCapture(cam_id)
-        if not cap.isOpened():
-            print('Error opening input video: {}'.format(cam_id))
-            continue
-        # print('{} opened'.format(cam_id))
-        caps.append(cap)
-        if len(caps) == cameras_count:
-            break
-    return caps
-
-
-class Camera(SingletonConfigurable):
+class CSICamera(SingletonConfigurable):
     value = traitlets.Any()
 
-    def __init__(self, cap, capture_size):
-        super(Camera, self).__init__()
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, capture_size['width'])
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, capture_size['height'])
-        self.value = np.empty((capture_size['height'], capture_size['width'], 3), dtype=np.uint8)
-        self.cap = cap
+    def __init__(self, capture_width, capture_height, image_width, image_height, fps=5):
+        super(CSICamera, self).__init__()
+        self.capture_width = capture_width
+        self.capture_height = capture_height
+        self.image_width = image_width
+        self.image_height = image_height
+        self.fps = fps
+        self.value = np.empty((image_width, image_height, 3), dtype=np.uint8)
         self.thread = threading.Thread(target=self._capture_frames)
         self.thread.start()
         atexit.register(self.stop)
 
+    def _get_csi_gsreamer_str(self):
+        result = 'nvarguscamerasrc ! ' \
+                 + 'video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! ' \
+                 + 'nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! ' \
+                 + 'appsink'
+        return result % (
+            self.capture_width,
+            self.capture_height,
+            self.fps,
+            self.image_width,
+            self.image_height
+        )
+
     def _capture_frames(self):
+        self.cap = cv2.VideoCapture(self._get_csi_gsreamer_str(), cv2.CAP_GSTREAMER)
+
         ts = None
         while True:
-            ts = keep_rps(ts, fps=5)
+            ts = keep_rps(ts, fps=self.fps)
 
             ret, frame = self.cap.read()
             if not ret:
@@ -55,32 +56,16 @@ class Camera(SingletonConfigurable):
             self.thread.join()
 
 
-def get_images_and_cameras(image_width=640, image_height=360):
-    capture_sizes = [  # FIXME
-        {'width': 1280, 'height': 720},
-        # {'width': 640, 'height': 360},
-        # {'width': 1280, 'height': 720},
-    ]
-
-    images = []
-    for _ in range(len(capture_sizes)):
-        image = widgets.Image(format='jpeg', width=image_width, height=image_height)
-        images.append(image)
-    if len(images) > 1:
-        sidebyside = widgets.HBox(images)
-    else:
-        sidebyside = images[0]
-
-    caps = get_captures(len(capture_sizes))
-    cameras = []
-    links = []
-    for i, image in enumerate(images):
-        cameras.append(Camera(caps[i], capture_sizes[i]))
-        links.append(
-            traitlets.dlink(
-                (cameras[-1], 'value'),
-                (image, 'value'),
-                transform=lambda x: bgr8_to_jpeg(cv2.resize(x, (image_width, image_height)))
-            )
-        )
-    return sidebyside, cameras
+def get_images_and_cameras(image_width=640, image_height=480, fps=5):
+    image = widgets.Image(format='jpeg', width=image_width, height=image_height)
+    camera = CSICamera(
+        capture_width=3264, capture_height=2464,
+        image_width=image_width, image_height=image_height,
+        fps=fps
+    )
+    traitlets.dlink(
+        (camera, 'value'),
+        (image, 'value'),
+        transform=lambda x: bgr8_to_jpeg(x),
+    )
+    return image, camera
