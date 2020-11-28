7dc1f3bdd29f47ee0edf20ebf7ac0b98cd71e634
Oleg Nurtdinov <j@jumper.cc>
2020-11-28 22:43:03 +0300

    make ros modules as script

--- scripts/make-ros-modules.sh
+++ scripts/make-ros-modules.sh
@@ -5,5 +5,9 @@ cd ~/catkin_ws/
 source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
+rm -rf src/ballsbot_detection && \
+cd src && \
+ln -s /home/ballsbot/projects/ballsbot/ballsbot_detection && \
+cd .. && \
 catkin_make
 
