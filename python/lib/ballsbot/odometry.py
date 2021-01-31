8066a0d247cc5df1c57c0937d5438ddd7eaf5a93
Oleg Nurtdinov <j@jumper.cc>
2021-01-31 15:01:43 +0300

    better odometry

--- python/lib/ballsbot/odometry.py
+++ python/lib/ballsbot/odometry.py
@@ -1,6 +1,6 @@
 from math import sin, cos, pi
 from time import time
-from ballsbot.utils import run_as_thread
+from ballsbot.utils import run_as_thread, keep_rps
 from ballsbot.config import ODOMETRY_METERS_PER_ROTATION, ODOMETRY_PRIMARY_PIN, ODOMETRY_SECONDARY_PIN
 
 
@@ -16,6 +16,15 @@ class Odometry:
         self.odometry_counter = 0
         self.direction = 0.
         self.odometry_last_interval = None
+
+        self.event_timestamps = {
+            'primary_rising': None,
+            'primary_falling': None,
+            'secondary_rising': None,
+            'secondary_falling': None,
+            'any_event': None,
+        }
+
         run_as_thread(self.update_odometry_cycle)
 
     def get_dx_dy(self, dt, teta):
@@ -68,21 +77,62 @@ class Odometry:
         GPIO.setup(ODOMETRY_PRIMARY_PIN, GPIO.IN)
         GPIO.setup(ODOMETRY_SECONDARY_PIN, GPIO.IN)
 
+        detections = self.event_timestamps
+
+        def primary_both(_):
+            if GPIO.input(ODOMETRY_PRIMARY_PIN) == GPIO.HIGH:
+                detections['primary_rising'] = time()
+                detections['any_event'] = detections['primary_rising']
+            else:
+                ts = time()
+                if detections['primary_falling'] is not None:
+                    prev_ts = detections['primary_falling']
+                    an_interval = ts - prev_ts
+                    if 0.05 < an_interval < 2.:
+                        self.odometry_last_interval = an_interval
+                detections['primary_falling'] = ts
+                detections['any_event'] = detections['primary_falling']
+
+        def secondary_both(_):
+            if GPIO.input(ODOMETRY_SECONDARY_PIN) == GPIO.HIGH:
+                detections['secondary_rising'] = time()
+                detections['any_event'] = detections['secondary_rising']
+                self.odometry_counter += 1
+            else:
+                detections['secondary_falling'] = time()
+                detections['any_event'] = detections['secondary_falling']
+
         try:
-            prev_ts = time()
+            GPIO.add_event_detect(ODOMETRY_PRIMARY_PIN, GPIO.BOTH, callback=primary_both)
+            GPIO.add_event_detect(ODOMETRY_SECONDARY_PIN, GPIO.BOTH, callback=secondary_both)
+
+            ts = None
             while True:
-                if GPIO.wait_for_edge(ODOMETRY_PRIMARY_PIN, GPIO.FALLING, timeout=750) is None:
-                    ts = time()
+                ts = keep_rps(ts, fps=2)
+                if detections['any_event'] is None or time() - detections['any_event'] > 0.5:
+                    # reset state if no events (car stopped)
                     self.odometry_last_interval = None
                     self.direction = 0.
-                else:
-                    self.direction = 1. if GPIO.input(ODOMETRY_SECONDARY_PIN) == GPIO.HIGH else -1.
-                    self.odometry_counter += 1
-                    ts = time()
-                    if 0.01 < ts - prev_ts < 10.:
-                        self.odometry_last_interval = ts - prev_ts
-                prev_ts = ts
+                    for k in detections.keys():
+                        detections[k] = None
+                elif detections['primary_falling'] is not None and detections['secondary_rising'] is not None \
+                        and (detections['primary_rising'] is not None or detections['secondary_falling'] is not None):
+                    # select events to determine direction
+                    select_rising = False
+                    if detections['primary_rising'] is not None and detections['secondary_falling'] is not None:
+                        if abs(detections['primary_rising'] - detections['secondary_rising']) < \
+                                abs(detections['primary_falling'] - detections['secondary_falling']):
+                            select_rising = True
+                        else:
+                            select_rising = False
+                    # determine direction
+                    if select_rising and detections['primary_rising'] is not None:
+                        self.direction = 1. if detections['primary_rising'] < detections['secondary_rising'] else -1.
+                    else:
+                        self.direction = 1. if detections['primary_falling'] < detections['secondary_falling'] else -1.
         finally:
+            GPIO.remove_event_detect(ODOMETRY_PRIMARY_PIN)
+            GPIO.remove_event_detect(ODOMETRY_SECONDARY_PIN)
             GPIO.cleanup()
 
     def get_speed(self):
