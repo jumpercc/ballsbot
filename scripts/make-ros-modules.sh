89cd70f8df2a7dbb5442fe9fd464d17bc8807c19
Oleg Nurtdinov <j@jumper.cc>
2021-02-07 19:53:41 +0300

    run_all.sh

--- scripts/make-ros-modules.sh
+++ scripts/make-ros-modules.sh
@@ -6,8 +6,11 @@ source /opt/ros/melodic/setup.bash
 source devel/setup.bash
 
 rm -rf src/ballsbot_detection && \
+rm -rf src/ballsbot_laser_ranging_sensor && \
 cd src && \
 ln -s /home/ballsbot/projects/ballsbot/ballsbot_detection && \
+ln -s /home/ballsbot/projects/ballsbot/ballsbot_laser_ranging_sensor && \
 cd .. && \
+
 catkin_make
 
---
+++ scripts/run-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -z ${NO_MAKE_ROS_MODULES+x} ]; then
+    $DIR/make-ros-modules.sh
+fi
+
+$DIR/run-roscore.sh > ~/core.log 2>&1 &
+
+sleep 5s  # wait for roscore to run TODO replace with roslaunch
+
+$DIR/run-lidar.sh > ~/lidar.log 2>&1 &
+sleep 1
+
+$DIR/run-ballsbot_laser_sensor_front.sh > ~/sensor_front.log 2>&1 &
+sleep 1
+
+$DIR/run-ballsbot_laser_sensor_rear.sh > ~/sensor_rear.log 2>&1 &
+sleep 1
+
+if [ -z ${NO_DETECTION+x} ]; then
+    $DIR/run-ballsbot_detection.sh > ~/detection.log 2>&1 &
+    sleep 1
+fi
+
+$DIR/run-jupyter.sh
+
+kill %1 2>/dev/null
+kill %2 2>/dev/null
+kill %3 2>/dev/null
+kill %4 2>/dev/null
+
