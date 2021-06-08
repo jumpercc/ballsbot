0a45d7886c793689d800f12a88515cc405712b3e
Oleg Nurtdinov <j@jumper.cc>
2021-06-08 23:24:36 +0300

    draw lidar and manipulator images in anothre process

--- python/lib/ballsbot/drawing.py
+++ python/lib/ballsbot/drawing.py
@@ -5,7 +5,7 @@ from matplotlib.transforms import Affine2D
 from io import BytesIO
 
 from ballsbot.utils import figsize_from_image_size
-from ballsbot.config import LIDAR_CALIBRATION_WITHOUT_MANIPULATOR
+from ballsbot.config import LIDAR_CALIBRATION_WITHOUT_MANIPULATOR, ENABLE_MULTIPROCESSING
 
 
 def update_image_abs_coords(
@@ -84,7 +84,18 @@ def update_image_abs_coords(
 
 
 def update_picture_self_coords(image, lidar_points, self_position, only_nearby_meters, additional_points):
-    fig = Figure(figsize=figsize_from_image_size(image))
+    figsize = figsize_from_image_size(image)
+    params = [figsize, lidar_points, self_position, only_nearby_meters, additional_points]
+    if ENABLE_MULTIPROCESSING:
+        drawer = DrawInAnotherProcess.get_instance('get_picture_self_coords')
+        value = drawer.draw('get_picture_self_coords', *params)
+    else:
+        value = get_picture_self_coords(*params)
+    image.value = value
+
+
+def get_picture_self_coords(figsize, lidar_points, self_position, only_nearby_meters, additional_points):
+    fig = Figure(figsize=figsize)
     canvas = FigureCanvas(fig)
     ax = fig.gca()
 
@@ -111,32 +122,58 @@ def update_picture_self_coords(image, lidar_points, self_position, only_nearby_m
     canvas.draw()
     jpeg = BytesIO()
     canvas.print_jpg(jpeg)
-    image.value = jpeg.getvalue()
+    return jpeg.getvalue()
 
 
 class ManipulatorPoseDrawing:
     def __init__(self, widgets):
         self.projection_xy = widgets.Image(format='jpeg', width=400, height=400)
-        self.projection_yz = widgets.Image(format='jpeg', width=400, height=400)
         self.projection_xz = widgets.Image(format='jpeg', width=400, height=400)
-        self.stub_image = widgets.Image(format='jpeg', width=400, height=400)
-        row1 = widgets.HBox([self.projection_xy, self.stub_image])
-        row2 = widgets.HBox([self.projection_xz, self.projection_yz])
-        self.images_grid = widgets.VBox([row1, row2])
+        self.images_grid = widgets.HBox([self.projection_xy, self.projection_xz])
 
     def get_images(self):
         return self.images_grid
 
     def update_images(self, manipulator_pose, override_crop_half_size=None):
-        if override_crop_half_size:
-            crop_half_size = override_crop_half_size
-        else:
-            crop_half_size = 1.
+        images = {'xy': self.projection_xy, 'xz': self.projection_xz}
+        figsizes = {}
+        for image_type, image in images.items():
+            figsizes[image_type] = figsize_from_image_size(image)
+        params = [figsizes, manipulator_pose, override_crop_half_size]
 
-        x_points = []
-        y_points = []
-        z_points = []
-        for a_point in manipulator_pose['points']:
+        if ENABLE_MULTIPROCESSING:
+            drawer = DrawInAnotherProcess.get_instance('get_manipulator_drawing_images')
+            values = drawer.draw('get_manipulator_drawing_images', *params)
+        else:
+            values = get_picture_self_coords(*params)
+
+        for image_type, value in values.items():
+            images[image_type].value = value
+
+
+def get_manipulator_drawing_images(figsizes, manipulator_pose, override_crop_half_size=None):
+    if override_crop_half_size:
+        crop_half_size = override_crop_half_size
+    else:
+        crop_half_size = 1.
+
+    x_points = []
+    y_points = []
+    z_points = []
+    for a_point in manipulator_pose['points']:
+        if not override_crop_half_size:
+            crop_half_size = max(crop_half_size, *a_point)
+        x, y, z = a_point
+        x_points.append(x)
+        y_points.append(y)
+        z_points.append(z)
+
+    if manipulator_pose.get('claw_points'):
+        claw_points = (
+            manipulator_pose['points'][-1], manipulator_pose['claw_points'][0],
+            manipulator_pose['points'][-1], manipulator_pose['claw_points'][1],
+        )
+        for a_point in claw_points:
             if not override_crop_half_size:
                 crop_half_size = max(crop_half_size, *a_point)
             x, y, z = a_point
@@ -144,109 +181,144 @@ class ManipulatorPoseDrawing:
             y_points.append(y)
             z_points.append(z)
 
-        if manipulator_pose.get('claw_points'):
-            claw_points = (
-                manipulator_pose['points'][-1], manipulator_pose['claw_points'][0],
-                manipulator_pose['points'][-1], manipulator_pose['claw_points'][1],
+    if not override_crop_half_size:
+        crop_half_size *= 1.05
+
+    color = [
+        'orange',
+        'black',
+        'red',
+        'green',
+        'blue',
+        'blue',
+        'blue',
+        'blue',
+    ]
+
+    result = {}
+    for image_type, figsize in figsizes.items():
+        fig = Figure(figsize=figsize)
+        canvas = FigureCanvas(fig)
+        ax = fig.gca()
+
+        ax.text(-crop_half_size, -crop_half_size, image_type, fontsize=16)
+
+        if image_type == 'xy':
+            width = LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['fl_x'] - LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_x']
+            height = LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['fl_y'] - LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_y']
+            rect = patches.Rectangle(
+                (
+                    LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_x'] * 1000,
+                    LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_y'] * 1000
+                ),
+                width * 1000, height * 1000,
+                linewidth=2, edgecolor='black', facecolor='none'
             )
-            for a_point in claw_points:
-                if not override_crop_half_size:
-                    crop_half_size = max(crop_half_size, *a_point)
-                x, y, z = a_point
-                x_points.append(x)
-                y_points.append(y)
-                z_points.append(z)
+            ax.add_patch(rect)
 
-        if not override_crop_half_size:
-            crop_half_size *= 1.05
+        if image_type == 'xy':
+            first_points = x_points
+            second_points = y_points
+        elif image_type == 'xz':
+            first_points = x_points
+            second_points = z_points
+        else:
+            raise ValueError(f'unknown axis combination {image_type}')
 
-        color = [
-            'orange',
-            'black',
-            'red',
-            'green',
-            'blue',
-            'blue',
-            'blue',
-            'blue',
-        ]
+        prev_first = 0.
+        prev_second = 0.
+        for i in range(len(first_points)):
+            ax.plot(
+                [prev_first, first_points[i]],
+                [prev_second, second_points[i]],
+                c=color[i], linewidth=3
+            )
+            prev_first = first_points[i]
+            prev_second = second_points[i]
 
-        images = {'xy': self.projection_xy, 'xz': self.projection_xz, 'yz': self.projection_yz, '': self.stub_image}
-        for image_type, image in images.items():
-            fig = Figure(figsize=figsize_from_image_size(image))
-            canvas = FigureCanvas(fig)
-            ax = fig.gca()
+        ax.scatter(first_points, second_points, marker='o', s=15, c='b')
 
-            ax.text(-crop_half_size, -crop_half_size, image_type, fontsize=16)
+        ax.set_xlim(-crop_half_size, crop_half_size)
+        ax.set_ylim(-crop_half_size, crop_half_size)
+        ax.grid(which='both', linestyle='--', alpha=0.5)
 
-            if image_type == 'xy':
-                width = LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['fl_x'] - LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_x']
-                height = LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['fl_y'] - LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_y']
-                rect = patches.Rectangle(
-                    (
-                        LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_x'] * 1000,
-                        LIDAR_CALIBRATION_WITHOUT_MANIPULATOR['rr_y'] * 1000
-                    ),
-                    width * 1000, height * 1000,
-                    linewidth=2, edgecolor='black', facecolor='none'
-                )
-                ax.add_patch(rect)
+        canvas.draw()
+        jpeg = BytesIO()
+        canvas.print_jpg(jpeg)
+        result[image_type] = jpeg.getvalue()
 
-            if image_type == 'xy':
-                first_points = x_points
-                second_points = y_points
-            elif image_type == 'xz':
-                first_points = x_points
-                second_points = z_points
-            elif image_type == 'yz':
-                first_points = y_points
-                second_points = z_points
-            else:
-                first_points = []
-                second_points = []
-                ax.text(
-                    -crop_half_size, -crop_half_size,
-                    self.format_pose(manipulator_pose),
-                    fontsize=10, color='green'
-                )
+    return result
 
-            prev_first = 0.
-            prev_second = 0.
-            for i in range(len(first_points)):
-                ax.plot(
-                    [prev_first, first_points[i]],
-                    [prev_second, second_points[i]],
-                    c=color[i], linewidth=3
-                )
-                prev_first = first_points[i]
-                prev_second = second_points[i]
 
-            ax.scatter(first_points, second_points, marker='o', s=15, c='b')
+def format_pose(pose):
+    result = 'rotations:\n'
+    for it in pose['rotations']:
+        result += '    ({:0.02f}, {:0.02f}, {:0.02f})\n'.format(*it)
 
-            ax.set_xlim(-crop_half_size, crop_half_size)
-            ax.set_ylim(-crop_half_size, crop_half_size)
-            if image_type:
-                ax.grid(which='both', linestyle='--', alpha=0.5)
-            else:
-                ax.axis('off')
+    result += 'points:\n'
+    for it in pose['points']:
+        result += '    ({:0.0f}, {:0.0f}, {:0.0f})\n'.format(*it)
 
-            canvas.draw()
-            jpeg = BytesIO()
-            canvas.print_jpg(jpeg)
-            image.value = jpeg.getvalue()
+    result += 'claw_points:\n'
+    for it in pose['claw_points']:
+        result += '    ({:0.0f}, {:0.0f}, {:0.0f})\n'.format(*it)
+
+    return result
+
+
+if ENABLE_MULTIPROCESSING:
+    import multiprocessing as mp
+
+    if not mp.get_start_method(allow_none=True):
+        mp.set_start_method('spawn')
+
+
+class DrawInAnotherProcess:
+    @classmethod
+    def get_instance(cls, instance_name):
+        instances = getattr(cls, 'instances', None)
+        if instances is None:
+            instances = {}
+            setattr(cls, 'instances', instances)
+        if instance_name in instances:
+            instance = instances[instance_name]
+        else:
+            instance = cls(instance_name)
+            instances[instance_name] = instance
+        return instance
+
+    @classmethod
+    def stop_all(cls):
+        instances = getattr(cls, 'instances', None)
+        if instances is None:
+            return
+        for instance in instances.values():
+            instance.stop()
+        instances.clear()
+
+    def __init__(self, name):
+        self.conn, child_conn = mp.Pipe()
+        self.process = mp.Process(
+            name=f'DrawInAnotherProcess:{name}',
+            target=self._processing_cycle,
+            args=(child_conn,)
+        )
+        self.process.start()
+
+    def draw(self, function_name, *params):
+        self.conn.send([function_name, params])
+        return self.conn.recv()
+
+    def stop(self):
+        self.conn.send(['stop', []])
+        self.process.join()
 
     @staticmethod
-    def format_pose(pose):
-        result = 'rotations:\n'
-        for it in pose['rotations']:
-            result += '    ({:0.02f}, {:0.02f}, {:0.02f})\n'.format(*it)
-
-        result += 'points:\n'
-        for it in pose['points']:
-            result += '    ({:0.0f}, {:0.0f}, {:0.0f})\n'.format(*it)
-
-        result += 'claw_points:\n'
-        for it in pose['claw_points']:
-            result += '    ({:0.0f}, {:0.0f}, {:0.0f})\n'.format(*it)
-
-        return result
+    def _processing_cycle(conn):
+        while True:
+            function_name, params = conn.recv()
+            if function_name == 'stop':
+                conn.close()
+                break
+            result = globals()[function_name](*params)
+            conn.send(result)
