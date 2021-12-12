a7940cf0dcd51b7d2d9d493fec2c8681a2205a79
Oleg Nurtdinov <j@jumper.cc>
2021-12-12 19:39:44 +0300

    bugfix and configuration commands list draft

--- data/ballsbot_config_override.py
+++ data/ballsbot_config_override.py
@@ -22,7 +22,7 @@ CAR_CONTROLS = {
         "zero_pulse": 360,
         "max_pulse": 390,
         "default_position": 0.,
-        "control": {"axis": 1, "reverse": True},
+        "control": {"axis": 1, "invert": True},
         "turbo_control": {"button": 1},
     },
 }
