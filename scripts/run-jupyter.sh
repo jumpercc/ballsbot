e54b4278386929e1979119d41e51ae6db77a8f07
Oleg Nurtdinov <j@jumper.cc>
2022-02-19 20:24:59 +0300

    jupyter on local port support added

--- scripts/run-jupyter.sh
+++ scripts/run-jupyter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-MY_IP=$(ifconfig | grep -P '\binet\b' | grep -vF 'inet 127.0.0.1' | head -1 | awk '{print $2}');
+if [ -z ${LOCAL_PORT+x} ]; then
+  MY_IP=$(ifconfig | grep -P '\binet\b' | grep -vF 'inet 127.0.0.1' | head -1 | awk '{print $2}')
+else
+  MY_IP=127.0.0.1
+fi
 
 jupyter lab --no-browser --notebook-dir=/home/ballsbot/notebooks --ip=$MY_IP
