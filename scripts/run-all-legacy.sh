78feea2974773d49658ee3cf6f05134402afe1c0
Oleg Nurtdinov <j@jumper.cc>
2022-06-04 21:28:18 +0300

    ups for legacy runner

--- scripts/run-all-legacy.sh
+++ scripts/run-all-legacy.sh
@@ -38,6 +38,11 @@ sleep 1
 $DIR/run-ballsbot_pose.sh > ~/pose.log 2>&1 &
 sleep 1
 
+if [ -z ${NO_UPS+x} ]; then
+  $DIR/run-ballsbot_ups.sh > ~/ups.log 2>&1 &
+  sleep 1
+fi
+
 if [ -z ${NO_DETECTION+x} ]; then
     $DIR/run-ballsbot_detection.sh > ~/detection.log 2>&1 &
     sleep 1
@@ -61,5 +66,6 @@ kill %4 2>/dev/null
 kill %5 2>/dev/null
 kill %6 2>/dev/null
 kill %7 2>/dev/null
+kill %8 2>/dev/null
 
 kill %1 2>/dev/null
